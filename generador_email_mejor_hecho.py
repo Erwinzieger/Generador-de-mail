0b531d9585245e32a11df4c1288eb102b5a2486f
Erwin Zieger <e-zieger@hotmail.com>
2024-12-16 03:45:27 -0300

    Add files via upload

---
+++ generador_email_mejor_hecho.py
@@ -0,0 +1,22 @@
+# Generador de email funcional!
+print('*** BIENVENDIDO AL GENERADOR DE MAIL DE MURDOK!')
+print()
+nombre = input('Introduzca su nombre: ')
+apellido = input('Introduzca su apellido: ')
+empresa = input('Introduzca el nombre de la empresa de su mail (hotmail,gmail,yahoo,etc): ')
+dominio = input('Introduzca el nombre del dominio (.com,.com.ar) ')
+
+nombre = nombre.lower().replace(' ','').strip()
+apellido = apellido.lower().replace(' ','').strip()
+empresa = empresa.lower().replace(' ','').strip()
+dominio = dominio.lower().replace(' ','').strip()
+
+print()
+print(f'Su correo de electronico generado es: {nombre}{apellido}@{empresa}{dominio}')
+print()
+
+nombre = nombre[:2]
+apellido = apellido[:2]
+
+print(f'Otra alternativa es: {nombre}{apellido}@{empresa}{dominio}')
+
